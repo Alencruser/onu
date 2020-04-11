b8e24dae0b59f009006353503f1e9259be35a2f4
unknown <molenda.lucas1@gmail.com>
2020-04-11 18:35:53 +0200

    Debug + Event + Fisrt test

--- test/card.js
+++ test/card.js
@@ -1,17 +1,19 @@
 let Games = require('../public/Games.js');
-
+let chai = require('chai');
+var expect = chai.expect;
+var Card = Games.Card;
 describe('Card', function () {
     describe('#constructor', function () {
         it('creates a valid card', () => {
-            expect(() => new Games.Card(Value.ZERO, Color.RED)).not.toThrow();
+            expect(() => new Card(Value.ZERO, Color.RED)).not.Throw();
         });
 
         it('creates wild cards with no color', () => {
-            expect(() => new Games.Card(Value.WILD)).not.toThrow();
+            expect(() => new Card(Value.WILD)).not.Throw();
         });
 
         it('creates wild cards with a color', () => {
-            expect(() => new Games.Card(Value.WILD, Color.RED)).not.toThrow();
+            expect(() => new Card(Value.WILD, Color.RED)).not.Throw();
         });
     });
 });
